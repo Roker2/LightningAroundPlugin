06193f5936c802ef594dfba8f38c251282328cb4
Roker2 <dimaminko2000@gmail.com>
2024-10-09 17:13:08 +0200

    RandomPlayerPicker: Init

---
+++ src/main/java/pl/roker2/lightning/RandomPlayerPicker.java
@@ -0,0 +1,18 @@
+package pl.roker2.lightning;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Random;
+
+public class RandomPlayerPicker {
+    public static Player getRandomPlayer() {
+        Collection<? extends Player> players = Bukkit.getServer().getOnlinePlayers();
+        int randomIndex = new Random().nextInt(players.size());
+        List<? extends Player> playerList = new ArrayList<>(players);
+        return playerList.get(randomIndex);
+    }
+}
