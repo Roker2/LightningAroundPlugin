6ef35c26bb5ee5cf0273679da9cf00ec60527e12
Roker2 <dimaminko2000@gmail.com>
2024-10-11 00:18:30 +0200

    RandomPlayerPicker: Check collection size

--- src/main/java/pl/roker2/lightning/RandomPlayerPicker.java
+++ src/main/java/pl/roker2/lightning/RandomPlayerPicker.java
@@ -11,6 +11,8 @@ import java.util.Random;
 public class RandomPlayerPicker {
     public static Player getRandomPlayer() {
         Collection<? extends Player> players = Bukkit.getServer().getOnlinePlayers();
+        if (players.isEmpty())
+            return null;
         int randomIndex = new Random().nextInt(players.size());
         List<? extends Player> playerList = new ArrayList<>(players);
         return playerList.get(randomIndex);
